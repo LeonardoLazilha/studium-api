ea24b4625ab9f24aeb1c80ee9dcfa30d87414123
Leonardo Lazilha <leonardolazilha@MacBook-Pro-de-Leonardo.local>
2025-01-24 21:34:21 -0300

    first commit

---
+++ src/test/java/com/studium/StudiumstudiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiumstudiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
